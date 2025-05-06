fc2730c83a2bc76ed8fa4d9755eac702a0cb8e8d
Bryce Klinker <bryce.klinker@upliftdelivery.com>
2025-05-06 08:54:46 -0500

    adjusting build to assume sentry is external

--- tsup.base.ts
+++ tsup.base.ts
@@ -1,15 +1,16 @@
 import {Options, defineConfig} from 'tsup'
 import {dependencies} from './layers/nodejs/package.json';
 
-const layerDependencies = Object.keys(dependencies);
+const layerDependencies = Object.keys(dependencies)
+    .join('|');
 export function fromTsupBase(overrides?: Partial<Options>): ReturnType<typeof defineConfig> {
     return defineConfig({
         clean: true,
         minify: true,
         target: 'node20',
         sourcemap: true,
-        external: [...layerDependencies],
-        noExternal: layerDependencies.map(l => new RegExp(`^(?!(${l}))`)),
+        external: [new RegExp(`^(${layerDependencies})`)],
+        noExternal: [new RegExp(`^(?!(${layerDependencies}))`)],
         ...overrides,
     })
 }
